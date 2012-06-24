2c74fbb09ccd7218a16af9bc65f968e5c40b78fb
Mary Cutrali <mary.cutrali@gmail.com>
2012-06-24 14:31:54 -0400

    add scaffold for invites

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InviteMe::Application.config.secret_token = 'c454b2d822564d739a2c42c96f323fd98721ef1a09014fbe0687a1eb89eeaabd6b9691fd1e5e41a1f27f0880e627f4a19a1970352b9986d1272abd36f3704d61'
